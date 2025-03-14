3fcf6464bdd40e2ace5fedd7502e375202a72053
Anas <manasgithub1@gmail.com>
2025-03-14 11:42:48 +0000

    chore: add delete preset option with context menu

--- features/editor/components/editor-sidebar/tabs/config-tab/presets/preset-items.tsx
+++ features/editor/components/editor-sidebar/tabs/config-tab/presets/preset-items.tsx
@@ -7,13 +7,24 @@ import { COMPONENTS_TYPES_ICONS } from '../../../constants';
 //@TYPES
 import { Preset } from '@/features/editor/types';
 import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip';
+import {
+    ContextMenu,
+    ContextMenuContent,
+    ContextMenuItem,
+    ContextMenuShortcut,
+    ContextMenuTrigger,
+} from '@/components/ui/context-menu';
+import { Trash } from 'lucide-react';
+import { useModal } from '@/components/providers/modal-provider';
+import GenericModal from '@/components/generic-modal';
 
 type Props = {
     items: Preset[];
 };
 
 export default function PresetItems({ items }: Props) {
-    const { handleDragComponent } = useEditor();
+    const { presets, handleDragComponent, updateCourseData } = useEditor();
+    const { handleSetModal } = useModal();
 
     const handleDragStart = (e: React.DragEvent, type: string, presetName: string) => {
         e.dataTransfer.setData('type', type);
@@ -31,22 +42,52 @@ export default function PresetItems({ items }: Props) {
         );
     };
 
+    const handleDeletePreset = (name: string) => {
+        handleSetModal(
+            <GenericModal
+                title="Delete Preset"
+                description="Are you sure you want to delete this preset?"
+                btn_text={'Delete'}
+                btn_action={() =>
+                    updateCourseData(
+                        'presets',
+                        presets.filter((p) => p.name !== name),
+                    )
+                }
+            />,
+        );
+    };
+
     return items.map((item, i) => {
         const Icon = COMPONENTS_TYPES_ICONS[item.type];
         return (
-            <Tooltip key={i}>
-                <TooltipTrigger asChild>
-                    <div
-                        className="mb-2 flex w-[80px] cursor-pointer flex-col items-center gap-2 rounded-md bg-accent py-4 text-accent-foreground transition-all hover:bg-muted hover:text-muted-foreground"
-                        draggable
-                        onDragStart={(e) => handleDragStart(e, item.type, item.name)}
+            <ContextMenu>
+                <ContextMenuTrigger>
+                    <Tooltip key={i}>
+                        <TooltipTrigger asChild>
+                            <div
+                                className="mb-2 flex w-[80px] cursor-pointer flex-col items-center gap-2 rounded-md bg-accent py-4 text-accent-foreground transition-all hover:bg-muted hover:text-muted-foreground"
+                                draggable
+                                onDragStart={(e) => handleDragStart(e, item.type, item.name)}
+                            >
+                                <Icon size={24} />
+                                <p className="max-w-[60px] truncate text-center text-xs">{item.name}</p>
+                            </div>
+                        </TooltipTrigger>
+                        <TooltipContent>{item.name}</TooltipContent>
+                    </Tooltip>
+                </ContextMenuTrigger>
+                <ContextMenuContent className="w-64">
+                    <ContextMenuItem
+                        inset
+                        className="flex cursor-pointer items-center justify-between gap-2"
+                        onClick={() => handleDeletePreset(item.name)}
                     >
-                        <Icon size={24} />
-                        <p className="max-w-[60px] truncate text-center text-xs">{item.name}</p>
-                    </div>
-                </TooltipTrigger>
-                <TooltipContent>{item.name}</TooltipContent>
-            </Tooltip>
+                        Delete
+                        <Trash size={16} />
+                    </ContextMenuItem>
+                </ContextMenuContent>
+            </ContextMenu>
         );
     });
 }
