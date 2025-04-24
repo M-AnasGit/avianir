36929d40afeceb245ca8d260006e120b004c385b
Anas <manasgithub1@gmail.com>
2025-04-24 22:16:29 +0100

    chore: replace placeholders with logo

--- app/auth/_components/auth-header.tsx
+++ app/auth/_components/auth-header.tsx
@@ -1,4 +1,4 @@
-import { GalleryHorizontalIcon } from 'lucide-react';
+import Image from 'next/image';
 
 type Props = {
     title: string;
@@ -10,7 +10,7 @@ export default function AuthHeader({ title, description }: Props) {
         <div className="flex flex-col items-center gap-1 md:gap-2">
             <span className="flex flex-col items-center gap-2 font-medium">
                 <div className="flex items-center justify-center">
-                    <GalleryHorizontalIcon className="h-8 w-8 text-primary" />
+                    <Image src={`/vector.png`} alt="Logo" width={64} height={64} />
                 </div>
                 <span className="sr-only">{process.env.NEXT_PUBLIC_PLACEHOLDER_NAME}</span>
             </span>
