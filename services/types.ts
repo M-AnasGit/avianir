587fe081f1d91183dad9191f6d1dc9911be7a03a
Anas <manasgithub1@gmail.com>
2025-05-10 21:27:10 +0100

    refactor: refactor types

--- services/types.ts
+++ services/types.ts
@@ -1,3 +1,31 @@
+import { Database } from '@/db/database.types';
+
+export type User = Database['public']['Tables']['user']['Row'];
+
+export type UserDetails = {
+    city?: string | null;
+    occupation?: string | null;
+    socials: {
+        github?: string | undefined;
+        twitter?: string | undefined;
+        linkedin?: string | undefined;
+        instagram?: string | undefined;
+        website?: string | undefined;
+        facebook?: string | undefined;
+    };
+    description?: string | null;
+    languages: string[];
+    fields: string[];
+};
+
+export type UserWithDetails = Omit<User, 'details'> & {
+    details: UserDetails;
+};
+
+export type UserWithEmail = UserWithDetails & {
+    email: string;
+};
+
 export type MediaType = 'image' | 'video' | 'audio';
 
 export type Media = {
